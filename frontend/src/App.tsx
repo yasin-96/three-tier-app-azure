e4fc882b65838b3867fb91b22d8a5f1bcdf9c2b1
Yasin Eraslan <yasineraslan@Yasins-MBP.fritz.box>
2026-09-10 11:48:13 +0200

    deploy react frontend to front door

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const sendRequest = async () => {
     setLoading(true)
     try {
-      await fetch('https://backend-lb-310342544.eu-west-1.elb.amazonaws.com/api/hello', {
+      await fetch('http://three-tier-api.germanywestcentral.cloudapp.azure.com/api/hello', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
